0863c8ff7df69b3462b38365d80feb20c07a00fb
chengwei luo <luo.chengwei@icloud.com>
2013-12-26 18:18:17 -0500

    fix bug

--- src/metaHGT.py
+++ src/metaHGT.py
@@ -187,7 +187,8 @@ class ProjectInfo:
 	def initProject(self, options):
 		if os.path.exists(options.sample_list):
 			for timepoint in open(options.sample_list, 'r'):
-				self.samples.append(tuple(timepoint.rstrip('\n').split(':')))
+				print timepoint[-1], timepoint[-1].split(':')
+				self.samples.append(tuple(timepoint[-1].split(':')))
 		elif options.sample_list.count(',') > 0:
 			for timepoint in options.sample_list.split(','):
 				self.samples.append(tuple(timepoint.split(':')))
